df16fa52556195401fa6ae1038426eba4efacd07
Kiarosh Moeini <kmoeini@gmail.com>
2024-03-06 13:19:20 +0900

    feat:user endpoints finished

--- controllers/users.controller.ts
+++ controllers/users.controller.ts
@@ -4,38 +4,31 @@ import { Request, Response } from "express";
 const usersController = {
 
   getById: async function (req: Request, res: Response) {
-    const userid = req.body["user_id"];
-
-    // let results = await usersModel.getById(username);
-
-    // res.status(200).send(results);
+    try {
+      const id: number = parseInt(req.params.id);
+      const user = await usersModel.getById(id);
+      res.status(200).send(user);
+    } catch (e) {
+      console.log(e);
+    }
   },
 
   create: async function (req: Request, res: Response) {
     const userData = req.body;
     const newUser = usersModel.create(userData);
     res.send(newUser);
-    // SEND RESPONSE
-   
-      //***********VALIDATE INPUT LATER***********
-      // if (!validateUsers(newUsers)) {
-      //   res.status(400).send("Invalid input");
-      // } else {
-        // let results = await usersModel.create(newUser);
-        // if (results.hasOwnProperty("createdId"))
-        //   res.status(200).send(results);
-        // else
-        //   res.status(500).send("Something went wrong");
-      },
+  },
 
-      update: async function (req: Request, res: Response) {
-        const id = req.params.id;
-        const entry = req.body;
-    
-        // let result = await usersModel.update(id, entry);
-    
-        // res.status(200).send(result);
-      },
-    
+  update: async function (req: Request, res: Response) {
+    try {
+      const id: number = parseInt(req.params.id);
+      const dataToUpdate = req.body;
+      const updatedUser = await usersModel.update(id, dataToUpdate);
+      res.status(200).send(updatedUser);
+    } catch (e) {
+      console.log(e);
+    }
+  },
+  
 }
 export default usersController;
--- models/users.model.ts
+++ models/users.model.ts
@@ -17,43 +17,23 @@ const usersModel = {
       return user;
     },
 
-  create: async function (userData: User) {
-    // WRITE CODE TO CREATE NEW USER IN DATABASE
-    // const newUser = await prisma.user.create({ data: { userData } });
-    // return newUser;
-      try {
-        const newUser = await prisma.user.create({
-          data: userData,
-        });
-        console.log('Successfully created user:', newUser);
-        return newUser;
-      } catch (error) {
-        console.error('Error creating user:', error);
-        throw error;
-      } finally {
-        await prisma.$disconnect();
-      }
-    },
+    create: async function (user: User) {
+      const newUser = await prisma.user.create({
+        data: user,
+      });
   
- 
-
-  update: async function (userId: number, updateData: object) {
-    // WRITE CODE TO UPDATE A USER IN DATABASE BY ID
-      try {
-        const updatedUser = await prisma.user.update({
-          where: {
-            id: userId,
-          },
-          data: updateData,
-        });
-        //console.log('User updated:', updatedUser);
-        return updatedUser;
-      } catch (error) {
-        console.error('Error updating user');
-        throw error; 
-      }
-    }
-
+      return newUser;
+    },
+    
+    update: async function (id: number, data: object) {
+      const updatedUser = await prisma.user.update({
+        where: {
+          id: id,
+        },
+        data: data,
+      })
+      return updatedUser;
+    },
   }
 
 export default usersModel;
