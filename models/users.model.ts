efd7a48b7ce1ee321c1a8e720fdf237f6147ba78
yurikahirata <yurikahirata0130@gmail.com>
2024-03-06 15:25:02 +0900

    fix: await create user endpoint

--- models/users.model.ts
+++ models/users.model.ts
@@ -4,36 +4,38 @@ const prisma = new PrismaClient()
 interface User {
   user_name: string,
   uuid: string,
+  img_id?: number,
+  display_name?: string;
 }
 
 const usersModel = {
   getById: async function (userId: number) {
     // WRITE CODE TO GET A USER IN DATABASE BY ID
-      const user = await prisma.user.findUnique({
-        where: {
-          id: userId,
-        },
-      });
-      return user;
-    },
+    const user = await prisma.user.findUnique({
+      where: {
+        id: userId,
+      },
+    });
+    return user;
+  },
 
-    create: async function (user: User) {
-      const newUser = await prisma.user.create({
-        data: user,
-      });
-  
-      return newUser;
-    },
-    
-    update: async function (id: number, data: object) {
-      const updatedUser = await prisma.user.update({
-        where: {
-          id: id,
-        },
-        data: data,
-      })
-      return updatedUser;
-    },
-  }
+  create: async function (user: User) {
+    const newUser = await prisma.user.create({
+      data: user,
+    });
+
+    return newUser;
+  },
+
+  update: async function (id: number, data: object) {
+    const updatedUser = await prisma.user.update({
+      where: {
+        id: id,
+      },
+      data: data,
+    })
+    return updatedUser;
+  },
+}
 
 export default usersModel;
