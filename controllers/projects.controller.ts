01c682c58786c4fd07bb568ec029f4e3b2e19062
yurikahirata <yurikahirata0130@gmail.com>
2024-03-06 11:42:04 +0900

    feat: projects model

--- controllers/projects.controller.ts
+++ controllers/projects.controller.ts
@@ -1,39 +1,50 @@
 import projectsModel from "../models/projects.model";
+import { Request, Response } from "express";
+
+interface Project {
+  title?: string | null;
+  description?: string | null;
+  img_id?: number | null;
+  user_id: number;
+  // updated_date: Date;
+}
+
+// interface ResponseBody { }
+
+// interface RequestBody { }
 
 const projectsController = {
-
-  getById: async function () {
-    // CODE PARSING REQUEST PARAMS
-    // MAYBE VALIDATION?
-    const project = projectsModel.getById();
-    // SEND RESPONSE
+  getById: async function (req: Request, res: Response) {
+    const id: number = parseInt(req.params.id);
+    const project = projectsModel.getById(id);
+    res.status(200).send(project);
   },
 
-  create: async function () {
-    // CODE PARSING REQUEST BODY
-    // MAYBE VALIDATION?
-    const newProject = projectsModel.create();
-    // SEND RESPONSE
+  create: async function (req: Request, res: Response) {
+    const newProjectToInsert = req.body;
+    newProjectToInsert["updated_date"] = new Date();
+    const newProject = projectsModel.create(newProjectToInsert);
+    res.status(200).send(newProject);
   },
 
   update: async function () {
     // CODE PARSING REQUEST PARAMS AND BODY
     // MAYBE VALIDATION?
-    const updatedProject = projectsModel.update();
+    // const updatedProject = projectsModel.update();
     // SEND RESPONSE
   },
 
   delete: async function () {
     // CODE PARSING REQUEST PARAMS
     // MAYBE VALIDATION?
-    projectsModel.delete();
+    // projectsModel.delete();
     // SEND RESPONSE
   },
 
   getByUserId: async function () {
     // CODE PARSING REQUEST PARAMS
     // MAYBE VALIDATION?
-    const entries = projectsModel.getByUserId();
+    // const entries = projectsModel.getByUserId();
     // SEND RESPONSE
   }
 }
--- models/projects.model.ts
+++ models/projects.model.ts
@@ -1,25 +1,69 @@
 import { Prisma, PrismaClient } from '@prisma/client'
 const prisma = new PrismaClient()
 
+type Project = {
+  title: string | null;
+  description: string | null;
+  img_id: number | null;
+  user_id: number;
+  updated_date: Date;
+}
+
 const projectsModel = {
-  getById: async function () {
+  getById: async function (id: number) {
     // WRITE CODE TO GET A PROJECT IN DATABASE BY ID
+    const project = await prisma.project.findUnique({
+      where: { id: id, },
+    });
+
+    return project;
   },
 
-  create: async function () {
+  create: async function (project: Project) {
     // WRITE CODE TO CREATE NEW PROJECT IN DATABASE
+    const newProject = await prisma.project.create({
+      data: project,
+    });
+
+    return newProject;
   },
 
-  update: async function () {
+  update: async function (id: number, data: any) {
     // WRITE CODE TO UPDATE AN PROJECT IN DATABASE BY ID
+    const updatedProject = await prisma.project.update({
+      where: {
+        id: id,
+      },
+      data: data,
+    })
+
+    return updatedProject;
   },
 
-  delete: async function () {
+  delete: async function (id: number) {
     // WRITE CODE TO DELETE AN PROJECT IN DATABASE BY ID
+    const deleteEntries = await prisma.entry.deleteMany({
+      where: {
+        project_id: id,
+      },
+    });
+
+    const deleteProject = await prisma.project.delete({
+      where: {
+        id: id,
+      },
+    });
   },
 
-  getByUserId: async function () {
+  getByUserId: async function (userId: number) {
     // WRITE CODE TO GET PROJECTS IN THE DATABASE BY USER ID
+    const projects = await prisma.project.findMany({
+      where: {
+        user_id: userId,
+      },
+    });
+
+    return projects;
   }
 
 }
