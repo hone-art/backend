ff108115949048506a588db0979837121e89095e
JiaxianGu <gujiaxian1997@gmail.com>
2024-03-12 23:12:24 +0900

    chore: clear console.log

--- utils/generateOneMonthContributions.ts
+++ utils/generateOneMonthContributions.ts
@@ -10,27 +10,10 @@ type Entry = {
 export function generateOneMonthContributions(numberOfDays: number, entries: Entry[]) {
     const result: number[] = new Array(numberOfDays).fill(0);
     
-    
-    // console.log(entries[0]);
-    // console.log(typeof entries[].created_date);
-    // entries.forEach(entry => entry.created_date.setHours(entry.created_date.getHours() + 9));
     for (let i = 0; i < entries.length; i++) {
-        console.log("original time is====",entries[i].created_date);
         const UTCTime = entries[i].created_date.getUTCHours();
-        console.log("UTC time is=======",UTCTime);
-        // const localTime = entries[i].created_date.getHours();
-        // console.log("Local time is ========", localTime);
         entries[i].created_date.setUTCHours(UTCTime + 9);
-        console.log("UTC time is=======",entries[i].created_date.getUTCHours());
-
-        
-        console.log(entries[i].created_date.getUTCDate());
         result[entries[i].created_date.getUTCDate()-1] += 1;
     }
-    // console.log(entries[0].created_date.getDate())
-    // for (let i = 0; i < entries.length; i++){
-    //     const day = entries[i].created_date.getDate();
-    //     console.log(day);
-    // }
     return result;
 }
